c1bab469d97c14d66ffa23d209ebadbd2531faab
Frt1290 <mazlumkizilkaya60@gmail.com>
2026-04-16 00:52:52 +0300

    Add runtime configuration for FIRATFLIX

---
+++ public/runtime-config.js
@@ -0,0 +1,16 @@
+window.FIRATFLIX_RUNTIME_CONFIG = {
+  backendBaseUrl: '',
+  proxyMode: 'always',
+  forceProxyImages: true,
+  forceProxyStreams: true,
+  forceProxyMetadata: true,
+  defaultSource: {
+    type: 'm3u',
+    playlistUrl: 'http://bluvip.online:8080/get.php?username=Ahmt.1504&password=Hmt.1504&type=&type=m3u_plus&output=ts',
+    epgUrl: '',
+    baseUrl: '',
+    username: '',
+    password: '',
+    label: 'BLUVIP Default M3U'
+  }
+};
